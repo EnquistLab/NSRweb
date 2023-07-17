95740c4c7fcdd48498e35d63bb5d97dc17320891
gcgbarbosa <gcgbarbosa@gmail.com>
2023-07-17 15:43:49 -0700

    Change theme

--- src/theme.js
+++ src/theme.js
@@ -13,10 +13,10 @@ export const roboto = Roboto({
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#2F401E',
     },
     secondary: {
-      main: '#19857b',
+      main: '#8FA63F',
     },
     error: {
       main: red.A400,
