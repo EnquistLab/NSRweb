dfa2b3ebc178f2bb15e6d17ff0a1572577d5ed5b
Rethvick <furyassassinofficial@gmail.com>
2024-06-11 13:26:59 -0700

    Added More Pages

--- components/top-bar/top-bar.jsx
+++ components/top-bar/top-bar.jsx
@@ -14,15 +14,18 @@ import {
   Link,
 } from "@mui/material";
 
-
 import { ContainerLG } from "../";
 
 export function TopBar() {
   const menuLinks = [
     { href: '/', name: 'Home' },
     { href: '/map', name: 'Map' },
+    { href: '/about', name: 'About' },
+    { href: '/instructions', name: 'Instructions' },
+    { href: '/nsrapi', name: 'API' },
     { href: '/sources', name: 'Sources' },
     { href: '/cite', name: 'Cite' },
+    { href: '/data_dictionary', name: 'Data Dictionary' },
   ]
 
   // API version
@@ -36,39 +39,39 @@ export function TopBar() {
   }, []);
 
   return (
-    <AppBar position="static">
-      <ContainerLG>
-        <Toolbar>
-          <Box mr={2}>
-            {/* <img height="40" src="/logo.png" /> */}
-            <GrassIcon />
-          </Box>
+      <AppBar position="static">
+        <ContainerLG>
+          <Toolbar>
+            <Box mr={2}>
+              <Link href="/" color="inherit" underline="none">
+                <img height="40" src="/favicon.ico" alt="Logo" />
+              </Link>
+            </Box>
 
-          <Link href="/" color="inherit" variant="h6" sx={{ pr: 2 }} underline="none">
-            NSR
-          </Link>
+            <Link href="/" color="inherit" variant="h6" sx={{ pr: 2 }} underline="none">
+              NSR
+            </Link>
 
-          <Box sx={{ flexGrow: 1 }}></Box>
+            <Box sx={{ flexGrow: 1 }}></Box>
 
-          <Box sx={{ display: { xs: 'none', lg: 'block' } }}>
-            <Typography variant="caption" >
-              Native Species Resolver {apiVersion}
-            </Typography>
-          </Box>
-          <Box sx={{ flexGrow: 1 }}></Box>
+            <Box sx={{ display: { xs: 'none', lg: 'block' } }}>
+              <Typography variant="caption" >
+                Native Species Resolver {apiVersion}
+              </Typography>
+            </Box>
+            <Box sx={{ flexGrow: 1 }}></Box>
 
-          <Box sx={{ display: { xs: 'none', md: 'block' } }}>
-            {menuLinks.map((item, k) =>
-              <Button key={k} color="inherit" href={item.href}>{item.name}</Button>
-            )}
-          </Box>
+            <Box sx={{ display: { xs: 'none', md: 'block' } }}>
+              {menuLinks.map((item, k) =>
+                  <Button key={k} color="inherit" href={item.href}>{item.name}</Button>
+              )}
+            </Box>
 
-          <Box sx={{ display: { sm: 'block', md: 'none' } }}>
-            <LowResBar menuLinks={menuLinks} />
-          </Box>
-        </Toolbar>
-      </ContainerLG>
-    </AppBar >
+            <Box sx={{ display: { sm: 'block', md: 'none' } }}>
+              <LowResBar menuLinks={menuLinks} />
+            </Box>
+          </Toolbar>
+        </ContainerLG>
+      </AppBar >
   );
 }
-
