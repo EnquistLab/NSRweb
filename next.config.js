806ac4322ba7c09c71beb18a7681fd38302bdf3d
Rethvick <furyassassinofficial@gmail.com>
2024-06-11 13:25:16 -0700

    More Lines Added

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  output: 'export',
   env: {
-    apiServer: "https://bien.nceas.ucsb.edu/nsr/",
+    apiServer: "https://nsrapi.xyz/",
     // production api
-    apiEndPoint: "https:bien.nceas.ucsb.edu/nsr/nsr_wsb.php",
+    apiEndPoint: "https://nsrapi.xyz/nsr_wsb.php"
+    //apiEndPoint: "https:bien.nceas.ucsb.edu/nsr/nsr_wsb.php",
     // development api
     // apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:9875/gnrs_api.php",
     // apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:9865/nsr_wsb.php",
